f41d1c7af4804406ff6950a5dc2700413746340f
RuBeeN-cmd <rollinruben@live.fr>
2024-05-28 10:52:23 +0200

    change rand func

--- srcs/ft_rand.c
+++ srcs/ft_rand.c
@@ -1,20 +1,19 @@
 #include "libft.h"
 
-int ft_rand32(void)
+int ft_rand(void *dest, size_t size)
 {
-	int	rand;
 	int fd = open("/dev/urandom", O_RDONLY);
 	if (fd < 0)
 	{
 		ft_printf_fd(2, "Failed to open /dev/urandom\n");
-		return (-1);
+		return (1);
 	}
-	if (read(fd, &rand, sizeof(rand)) < 0)
+	if (read(fd, dest, size) < 0)
 	{
 		ft_printf_fd(2, "Failed to read /dev/urandom\n");
 		close(fd);
-		return (-1);
+		return (1);
 	}
 	close(fd);
-	return (rand);
+	return (0);
 }
